422648c20aa8bce1923c4296721187058347d21d
zakaria-chahboun <zakaria.chahboun.1996@gmail.com>
2017-07-12 05:18:30 -0700

    add threading tutos

---
+++ 6- threading.py
@@ -0,0 +1,23 @@
+# always in the beginning :: for use print(..) like python 3
+from __future__ import print_function
+import threading
+import time
+
+def testTread():
+	for i in range(1,11):
+		print('{}/10'.format(i),end='\r') # override same line :)
+		time.sleep(1)
+
+def main():
+
+	MyTread = threading.Thread(target=testTread, name="MyTread")
+	
+	print('from {}'.format(MyTread.getName()))
+	MyTread.start()
+
+if __name__ == '__main__':	main()
+
+
+
+
+### zakaria chahboun | 2017
